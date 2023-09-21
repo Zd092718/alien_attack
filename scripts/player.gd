6c7a2cc6c067b7394844daaa83e356018f48826a
Zack Dowd <zdowd2796@gmail.com>
2023-09-21 10:07:26 -0600

    added rocket and movement fixes

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,20 @@
 extends CharacterBody2D
 
+var speed = 300
+
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+@onready var rocket_container = $RocketContainer #get_node("RocketContainer")
+ 
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		_shoot()
+	
 func _physics_process(delta):
+	# input actions for movement
 	velocity = Vector2(0,0)
-	var speed = 200
 	if Input.is_action_pressed("move_right"):
-		print("right button pressed")
 		velocity.x = speed
 	if Input.is_action_pressed("move_left"):
 		velocity.x = -speed
@@ -13,3 +23,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_down"):
 		velocity.y = speed
 	move_and_slide()
+	
+	# clamping movement to screen space
+	var screen_size = get_viewport_rect().size
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+
+func _shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
+	rocket_instance.global_position.x += 75
+	
