4e828fb6c578896cc49929539bf9d7fe870cb429
Zack Dowd <zdowd2796@gmail.com>
2023-09-22 09:24:50 -0600

    added game over screen

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,24 +2,34 @@ extends Node2D
 
 var lives = 3
 var score = 0
+var game_over_scene = preload("res://scenes/game_over_screen.tscn")
 
 @onready var player = $Player
 @onready var hud = $UI/HUD
+@onready var ui = $UI
 
 func _ready():
+	# sets score to zero on start
 	hud.set_score_label(score)
 
 func _on_deathzone_area_entered(area):
-	area.die()
+	# destroys enemies that pass player offscreen
+	area.queue_free()
 
 
 func _on_player_took_damage():
+	#deduct lives and set lives left label
 	lives -= 1
 	hud.set_lives_left_label(lives)
-	print(lives)
+	# check for death
 	if lives == 0:
-		print("Game Over")
 		player.die()
+		#adds delay timer before displaying game over screen
+		await get_tree().create_timer(1.5).timeout
+		#instantiate game over scene and set score
+		var gos_instance = game_over_scene.instantiate()
+		gos_instance.set_score(score)
+		ui.add_child(gos_instance)
 
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
@@ -27,6 +37,6 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	add_child(enemy_instance)
 	
 func _on_enemy_died():
+	#add score on enemy death
 	score += 100
 	hud.set_score_label(score)
-	print("Score: " + str(score))
